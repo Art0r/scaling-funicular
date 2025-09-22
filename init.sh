f7eb66c9aef4673d6ebe9b12e51aff33da80cc57
Arthur Fernandes <arthur0139@gmail.com>
2025-09-22 20:27:11 -0300

    feat: custom nginx

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 # Create the namespace if it doesn't exist
 kubectl create namespace scaling-funicular --dry-run=client -o yaml | kubectl apply -f -
 
+# applying nginx config
+kubectl create configmap nginx-config --from-file=nginx.conf=./nginx.conf -n scaling-funicular
+
 # Apply deployments and services to the namespace
 kubectl apply -f kubernetes/deployments/ -n scaling-funicular
 kubectl apply -f kubernetes/services/ -n scaling-funicular
