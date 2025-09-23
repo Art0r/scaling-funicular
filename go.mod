35faad0cc326a6c0fc8c9aa8057b6fe784dedef9
Arthur Fernandes <arthur0139@gmail.com>
2025-09-22 23:46:55 -0300

    feat: adding a golang service

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Art0r/scaling-funicular/hello_world_service
+
+go 1.24.5
--- init.sh
+++ init.sh
@@ -3,12 +3,24 @@
 # Create the namespace if it doesn't exist
 kubectl create namespace scaling-funicular --dry-run=client -o yaml | kubectl apply -f -
 
+# building hello_world_service
+cd hello_world_service || exit
+docker build -t scaling-funicular/hello_world_service . || exit
+cd ..
+
 # applying nginx config
-kubectl create configmap nginx-config --from-file=nginx.conf=./nginx.conf -n scaling-funicular
+kubectl create configmap nginx-config --from-file=nginx.conf=./nginx.conf -n scaling-funicular --dry-run=client -o yaml | kubectl apply -f -
 
 # Apply deployments and services to the namespace
 kubectl apply -f kubernetes/deployments/ -n scaling-funicular
 kubectl apply -f kubernetes/services/ -n scaling-funicular
+
+# Waiting for services to be ready
+kubectl wait --for=condition=ready pod -l app=database -n scaling-funicular --timeout=120s
+echo "Database ready"
+
+kubectl wait --for=condition=ready pod -l app=hello-world-service -n scaling-funicular --timeout=120s
+echo "hello-world-service ready"
 
 # Get the service URL
 minikube service nginx-service --url -n scaling-funicular
