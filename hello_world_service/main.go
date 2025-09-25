7b1b624303b855965c47edd89407a97498f84188
Arthur Fernandes <arthur0139@gmail.com>
2025-09-24 22:25:25 -0300

    feat: connecting to database

--- hello_world_service/main.go
+++ hello_world_service/main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	if os.Getenv("APP_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Printf("Warning: Error loading .env file: %v", err)
+		}
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	database := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, database)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -15,4 +47,3 @@ func main() {
 
 	log.Fatal(app.Listen(":3000"))
 }
-
